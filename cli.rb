964c9cccb481b7daa37d11ca9429fe4840c55b85
Chris Carter <chrisjohncarter123@gmail.com>
2020-11-14 21:30:20 -0600

    basic example

--- cli.rb
+++ cli.rb
@@ -1,2 +1,16 @@
+#ruby
 #!/usr/bin/env ruby
-puts "test"
+
+require 'optparse'
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: example.rb [options]"
+
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+end.parse!
+
+p options
+p ARGV
